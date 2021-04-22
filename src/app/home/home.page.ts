97416d48c2a147f218e835ceeeb5948a438b0022
theo <theo.cantin.benoist@gmail.com>
2021-04-22 10:26:32 +0200

    visuel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public eggsList = [
+    {
+      title:'coque',
+      image:'img/oeuf-coque.jpg'
+    },
+    {
+      title:'mollet',
+      image:'img/oeuf-mollet.jpg'
+
+    }
+
+  ]
+
+
 
   constructor() {}
 
