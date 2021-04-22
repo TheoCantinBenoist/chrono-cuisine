66200f5712ca8175ee2e7118b378a7d794e01999
theo <theo.cantin.benoist@gmail.com>
2021-04-22 11:33:34 +0200

    visuel et son

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,18 +9,33 @@ export class HomePage {
   public eggsList = [
     {
       title:'coque',
-      image:'img/oeuf-coque.jpg'
+      image:'img/oeuf-coque.jpg',
     },
     {
       title:'mollet',
-      image:'img/oeuf-mollet.jpg'
-
+      image:'img/oeuf-mollet.jpg', 
     }
-
   ]
+  public chosenEgg =null;
+  private audio:HTMLAudioElement = null;
+  public file = '/assets/son/bird.mp3'
 
 
+  public guessEgg(clickedEgg){
+   this.chosenEgg= clickedEgg
+  }
+  
 
   constructor() {}
+
+  public play(){
+   console.log(this.chosenEgg)
+    
+   this.audio =new Audio(this.file)
+   this.audio.load();
+   this.audio.play();
+
+  }
+
 
 }
