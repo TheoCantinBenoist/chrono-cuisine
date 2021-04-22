7347d2dbab28a218dbf1939f7ff6e9a2f20d91fe
theo <theo.cantin.benoist@gmail.com>
2021-04-22 15:05:58 +0200

    alarm fonctionne

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,32 +9,71 @@ import { Component } from '@angular/core';
 export class HomePage {
   public eggsList = [
     {
-      title:'coque',
-      image:'img/oeuf-coque.jpg',
+      title: 'coque',
+      image: 'img/oeuf-coque.jpg',
+      timer: 10,
     },
     {
-      title:'mollet',
-      image:'img/oeuf-mollet.jpg', 
+      title: 'mollet',
+      image: 'img/oeuf-mollet.jpg',
+      timer: 6,
     }
   ]
-  public chosenEgg =null;
-  private audio:HTMLAudioElement = null;
-  public file = '/assets/son/bird.mp3'
+  public chosenEgg = null;
+  private audio: HTMLAudioElement = null;
+  public file = '/assets/son/bird.mp3';
+  public secondsLeft = null;
+  public timer = null;
 
-
-  public guessEgg(clickedEgg){
-   this.chosenEgg= clickedEgg
+  public changebutton = true;
+  public guessEgg(clickedEgg) {
+    this.chosenEgg = clickedEgg
   }
-  
 
-  constructor() {}
 
-  public play(){
-   console.log(this.chosenEgg)
-    
-   this.audio =new Audio(this.file)
-   this.audio.load();
-   this.audio.play();
+  constructor(private toastCtrl: ToastController) { }
+
+
+  public action() {
+    if (this.changebutton) {
+      this.changebutton = !this.changebutton;
+      this.play()
+    } else {
+      this.stop()
+      this.changebutton = !this.changebutton;
+    }
+
+  }
+
+  public play() {
+    console.log(this.chosenEgg)
+
+    this.startTimer();
+  }
+
+  public stop() {
+    clearInterval(this.timer)
+    this.secondsLeft = 0;
+    this.chosenEgg=null;
+  }
+
+
+
+
+  private startTimer() {
+    this.secondsLeft = this.chosenEgg.timer
+    this.timer = setInterval(
+      () => {
+        this.secondsLeft--
+        console.log(this.secondsLeft)
+        if (this.secondsLeft == 0) {
+          this.audio = new Audio(this.file)
+          this.audio.load();
+          this.audio.play();
+          clearInterval(this.timer);
+        }
+      }, 1000
+    )
 
   }
 
